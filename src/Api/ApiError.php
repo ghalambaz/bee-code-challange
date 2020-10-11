4236fb962849a66cb16a6af025e6a678e10112f7
Ali Ghalambaz <aghalambaz@gmail.com>
2020-10-11 18:42:01 +0200

    creating specific ApiException for handling Api Errors

---
+++ src/Api/ApiError.php
@@ -0,0 +1,118 @@
+<?php
+
+
+namespace App\Api;
+
+
+class ApiError
+{
+    const TYPE_VALIDATION = 'validation_error';
+    const TYPE_PARSE_REQUEST_CONTENT = 'invalid_body_content_error';
+    const TYPE_INTERNAL = 'internal_error';
+    const TYPE_UNKNOWN = 'unknown_error';
+
+    private $defaultTitles = [
+        self::TYPE_VALIDATION => 'validation error',
+        self::TYPE_PARSE_REQUEST_CONTENT => 'we have some issue in parsing your request content',
+        self::TYPE_INTERNAL => 'internal server error',
+        self::TYPE_UNKNOWN => 'unknown error',
+    ];
+    /**
+     * @var int
+     */
+    private $statusCode;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var array
+     */
+    private $content = [];
+
+    /**
+     * ApiError constructor.
+     * @param int $statusCode
+     * @param string $type
+     * @param string|null $title
+     * @param array $content
+     */
+    public function __construct(
+        int $statusCode,
+        string $type = self::TYPE_UNKNOWN,
+        string $title = null,
+        array $content = []
+    ) {
+        if (is_null($title)) {
+            if (!empty($this->defaultTitles[$type])) {
+                $this->title = $this->defaultTitles[$type];
+            }
+        }
+        $this->statusCode = $statusCode;
+        $this->type = $type;
+        $this->title = $title;
+        $this->content = $content;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return ApiError
+     */
+    public function setTitle(string $title): ApiError
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getContent(): array
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $key
+     * @param $content
+     * @return ApiError
+     */
+    public function setContent(string $key, $content): ApiError
+    {
+        $this->content[$key] = $content;
+        return $this;
+    }
+
+}
